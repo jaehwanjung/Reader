6b885e90c6fae465ec942623c2b9169b76509318
jeffjung-ai <jeff.jung@aquaticinformatics.com>
2016-04-30 12:27:45 -0700

    feat: Add an angluar app for e reader

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Reader
+
+-----------------
+
+###Project Overview
 Create an AngularJS app for e-reader service
+
+### Learning Outcome
+* Intro to Angular Router
+
+###Resources
+* [Codecademy](http://www.codecademy.com/)
---
+++ js/controllers/BookController.js
@@ -0,0 +1,7 @@
+app.controller('BookController', ['$scope', '$routeParams','books', function($scope, $routeParams,books) {
+    books.success(function (data) {
+        $scope.book = data[$routeParams.bookId];
+    });
+    $scope.currentBookIndex = parseInt($routeParams.bookId);
+
+}]);
